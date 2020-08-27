479fe0864a3fbe4730e700f295e9890303a6c10f
Tai Nguyen <ntai9296@gmail.com>
2020-08-26 19:01:44 -0700

    google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,36 @@
-import Document from "next/document";
+import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-176586687-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-176586687-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
   static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
